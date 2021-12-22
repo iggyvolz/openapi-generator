c8b310eb497723e6f9763df2f96a02c8a0ce0689
Katie Volz <iggyvolz@gmail.com>
2021-12-21 22:13:11 -0500

    A little more progress on the generator

--- src/OpenapiGenerator.php
+++ src/OpenapiGenerator.php
@@ -9,8 +9,10 @@ use Nette\PhpGenerator\PhpNamespace;
 use Nette\Utils\Json;
 use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use RuntimeException;
 use Stringable;
@@ -42,9 +44,15 @@ abstract class OpenapiGenerator extends NamespacedClassGenerator
                 $method->addBody("\t\$this->endpoint . \"$methodPathRepl\",");
                 $method->addBody(')->withHeader("Accept", "application/json"));');
                 $method->addBody('switch($response->getStatusCode()) {');
+                $handledDefault = false;
                 foreach($operation["responses"] as $responseCode => $response) {
-                    $method->addBody($responseCode === "default" ? "\tdefault:" : "\tcase $responseCode:");
-                    $method->addBody("\t\tswitch(\$response->getHeader('content-type')[0]) {");
+                    if($responseCode === "default") {
+                        $handledDefault = true;
+                        $method->addBody("\tdefault:");
+                    } else {
+                        $method->addBody("\tcase $responseCode:");
+                    }
+                    $method->addBody("\t\tswitch((\$response->getHeader('content-type')??[])[0]??null) {");
                     if(array_key_exists("content", $response)) {
                         foreach ($response["content"] as $contentType => $content) {
                             $method->addBody("\t\t\tcase '$contentType':");
@@ -61,6 +69,10 @@ abstract class OpenapiGenerator extends NamespacedClassGenerator
                     }
                     $method->addBody("\t\t}");
                 }
+                if(!$handledDefault) {
+                    $method->addBody("\tdefault:");
+                    $method->addBody("\t\tthrow new \RuntimeException(\"Unexpected response code \" . \$response->getStatusCode());");
+                }
                 $method->addBody('}');
             }
         }
@@ -69,20 +81,32 @@ abstract class OpenapiGenerator extends NamespacedClassGenerator
     {
         $class->setAbstract();
         $class->addImplement(RequestHandlerInterface::class);
+        $constructor = $class->addMethod("__construct");
+        $constructor->addPromotedParameter("baseUrl")->setPrivate()->setType('string');
+        $constructor->addPromotedParameter("responseFactory")->setPrivate()->setType(ResponseFactoryInterface::class);
+        $constructor->addPromotedParameter("streamFactory")->setPrivate()->setType(StreamFactoryInterface::class);
         $handle = $class->addMethod("handle");
         $handle->addParameter("request")->setType(ServerRequestInterface::class);
         $handle->setReturnType(ResponseInterface::class);
         $value = Yaml::parse(FileSystem::read($this->getFile()));
+        $handle->addBody("if(str_starts_with(\$request->getUri()->getPath(), \$this->baseUrl)) {");
+        $handle->addBody("\t\$path = substr(\$request->getUri()->getPath(), strlen(\$this->baseUrl));");
+        $handle->addBody("} else {");
+        $handle->addBody("\treturn \$this->responseFactory->createResponse(404)->withBody(\$this->streamFactory->createStream(\"URL not found: \" . \$request->getUri()->getPath()));");
+        $handle->addBody("\tthrow new \\" . RuntimeException::class . "('URL not found: ' . \$request->getUri()->getPath());");
+        $handle->addBody("}");
         foreach($value["paths"] as $methodPath => $pathItem) {
-            $handle->addBody("if(preg_match(".var_export($this->pregify($methodPath), true).", \$request->getUri()->getPath(), \$matches)) {");
+            $handle->addBody("if(preg_match(".var_export($this->pregify($methodPath), true).", \$path, \$matches)) {");
             foreach($pathItem as $httpMethod => $operation) {
                 $handle->addBody("\tif(\strtolower(\$request->getMethod()) === " . var_export(strtolower($httpMethod), true) . ") {");
+//                $handle->addBody("\t\t/* ". json_encode($operation["responses"])." */");
                 $handle->addBody("\t\t" . $this->generateServerOperation($class->addMethod($operation["operationId"]), $operation, $methodPath));
+                $handle->addBody("\treturn \$this->responseFactory->createResponse(200);");
                 $handle->addBody("\t}");
             }
             $handle->addBody("}");
         }
-        $handle->addBody("throw new \\" . RuntimeException::class . "('Not implemented');");
+        $handle->addBody("return \$this->responseFactory->createResponse(404)->withBody(\$this->streamFactory->createStream(\"URL not found: \" . \$request->getUri()->getPath()));");
     }
     // Extract generation out to one method since multiple classes will need to be generated
     private function doGenerate(): void
@@ -138,32 +162,63 @@ abstract class OpenapiGenerator extends NamespacedClassGenerator
         if(array_key_exists("requestBody", $operation) && array_key_exists("application/json", $operation["requestBody"]["content"])) {
             // TODO handle more than JSON here
             // Maybe merge multiple content types
-            $required = $operation["requestBody"]["required"] ?? false;
+            $required = boolval($operation["requestBody"]["required"] ?? false);
             $schema = $operation["requestBody"]["content"]["application/json"]["schema"];
-            $body = $method->addParameter("body");
-            if(array_key_exists('$ref', $schema)) {
-                $type = $schema['$ref'];
-                $type = str_replace("#", $this->getNamespace(), $type);
-                $type = preg_replace("@/@", "\\", $type);
-                $this->generateSchema($type);
-                $body->setType($type);
-            } elseif($schema["type"] === "array") {
-                $body->setType('array');
-                $type = $schema["items"]['$ref'];
-                $type = str_replace("#", $this->getNamespace(), $type);
-                $type = preg_replace("@/@", "\\", $type);
-                $this->generateSchema($type);
-                $method->addComment("@param list<$type>" . ($required ? "" : "|null") . " \$body");
+            assert(is_array($schema));
+            list($type, $docblockType) = $this->generateSchema($schema);
+            $parameter = $method->addParameter("body");
+            $parameter->setType($type);
+            if(!is_null($docblockType)) {
+                $method->addComment("@param $docblockType \$body");
             }
-            if(!$required) {
-                $body->setNullable();
+            $parameter->setNullable(!$required);
+        }
+        if(array_key_exists("responses", $operation)) {
+            // Create abstract response class
+            $abstractResponseClassName = ucfirst($method->getName()."Response");
+            $method->setReturnType($this->getNamespace() . "\\responses\\" . $abstractResponseClassName);
+            $abstractResponseClassFile = new PhpFile();
+            $abstractResponseClass = $abstractResponseClassFile->addNamespace($this->getNamespace(). "\\responses")->addClass($abstractResponseClassName);
+            $abstractResponseClass->setAbstract();
+            $constructor = $abstractResponseClass->addMethod("__construct");
+            $constructor->addPromotedParameter("description")->setReadOnly()->setType("string");
+            $constructor->addPromotedParameter("responseCode")->setReadOnly()->setType("int");
+            $this->contents[$this->getNamespace() . "\\responses\\" . $abstractResponseClassName] = (new PsrPrinter())->printFile($abstractResponseClassFile);
+            class_exists($this->getNamespace() . "\\responses\\" . $abstractResponseClassName);
+            foreach($operation["responses"] as $code => $response) {
+                $responseClassName = $abstractResponseClassName . ucfirst($code);
+                $responseClassFile = new PhpFile();
+                $responseClass = $responseClassFile->addNamespace($this->getNamespace())->addClass($responseClassName);
+                $responseClass->setExtends($this->getNamespace() . "\\responses\\" . $abstractResponseClassName);
+                $responseClass->addConstant("DESCRIPTION", $response["description"] ?? "");
+                $constructor = $responseClass->addMethod("__construct");
+                $constructor->addComment(Yaml::dump($response));
+                $constructor->addBody("parent::__construct(");
+                $constructor->addBody("\tself::DESCRIPTION,");
+                if($code === "default") {
+                    $constructor->addParameter("code")->setType("int");
+                    $constructor->addBody("\t\$code,");
+                } else {
+                    $constructor->addBody("\t$code,");
+                }
+                $constructor->addBody(");");
+                if(array_key_exists("content", $response) && array_key_exists("application/json", $response["content"])) {
+                    [$type, $docblockType] = $this->generateSchema($response["content"]["application/json"]["schema"]);
+                    $constructor->addPromotedParameter("content")->setType($type);
+                    if(!is_null($docblockType)) {
+                        $constructor->addComment("@param $docblockType \$content");
+                    }
+                }
+                $this->contents[$this->getNamespace() . "\\responses\\" . $responseClassName] = (new PsrPrinter())->printFile($responseClassFile);
+                class_exists($this->getNamespace() . "\\responses\\" . $responseClassName);
             }
-//            $method->addComment(Yaml::dump($schema));
+        } else {
+            $method->setReturnType("void");
         }
         return $callingCode;
     }
 
-    private function generateSchema(string $type): void
+    private function generateRef(string $type): void
     {
         if(array_key_exists($type, $this->contents)) return;
         $file = new PhpFile();
@@ -179,7 +234,7 @@ abstract class OpenapiGenerator extends NamespacedClassGenerator
         $constructor = $class->addMethod("__construct");
         foreach($schema["properties"] as $name => $property) {
             $class->addComment("$name:\n\t" . str_replace("\n", "\n\t", Yaml::dump($property)));
-            $constructor->addPromotedParameter($name)->setPublic()->setReadOnly()->setType(match($property["type"]) {
+            $constructor->addPromotedParameter($name)->setPublic()->setReadOnly()->setType(match($property["type"] ?? null) {
                 "integer" => "int",
                 "string" => "string",
                 "boolean" => "bool",
@@ -188,5 +243,30 @@ abstract class OpenapiGenerator extends NamespacedClassGenerator
         }
         $this->contents[$type] = (new PsrPrinter())->printFile($file);
         self::autoload($type);
+    }
+
+    /**
+     * @param array $schema
+     * @return array{0:string,1:string}
+     */
+    private function generateSchema(array $schema): array
+    {
+        $docblockType = null;
+        if (array_key_exists('$ref', $schema)) {
+            $type = $schema['$ref'];
+            $type = str_replace("#", $this->getNamespace(), $type);
+            $type = preg_replace("@/@", "\\", $type);
+            $this->generateRef($type);
+        } elseif ($schema["type"] === "array") {
+            $listType = $schema["items"]['$ref'];
+            $listType = str_replace("#", $this->getNamespace(), $listType);
+            $listType = preg_replace("@/@", "\\", $listType);
+            $type = "array";
+            $docblockType = "list<$listType>";
+            $this->generateRef($listType);
+        } else {
+            $type = "mixed";
+        }
+        return [$type, $docblockType];
     }
 }
--- test/test.php
+++ test/test.php
@@ -1,11 +1,10 @@
 <?php
 
-use GuzzleHttp\Client;
+use iggyvolz\classgen\ClassGenerator;
 use Nyholm\Psr7\Factory\Psr17Factory;
 use PetStore\APIServer;
-use PetStore\components\schemas\Order;
-use PetStore\components\schemas\Pet;
-use PetStore\components\schemas\User;
+use PetStore\components\schemas;
+use PetStore\responses;
 use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -13,106 +12,106 @@ use Psr\Http\Message\ResponseInterface;
 require_once __DIR__ . "/../vendor/autoload.php";
 require_once __DIR__ . "/PetStoreGenerator.php";
 require_once __DIR__ . "/TestTransformer.php";
+//ClassGenerator::setMode(ClassGenerator::MODE_PRODUCTION);
 PetStoreGenerator::register();
-var_dump(class_exists(PetStore\APIClient::class));
-var_dump(class_exists(PetStore\APIServer::class));
-
-$apiServer = new class extends APIServer
+//spl_autoload(PetStore\APIClient::class);
+//spl_autoload(PetStore\APIServer::class);
+$psr17Factory = new Psr17Factory();
+$apiServer = new class("/api/v3",$psr17Factory, $psr17Factory) extends APIServer
 {
-    public function updatePet(Pet $body)
+    public function updatePet(schemas\Pet $body): responses\UpdatePetResponse
     {
         // TODO: Implement updatePet() method.
     }
 
-    public function addPet(Pet $body)
+    public function addPet(schemas\Pet $body): responses\AddPetResponse
     {
         // TODO: Implement addPet() method.
     }
 
-    public function findPetsByStatus()
+    public function findPetsByStatus(): responses\FindPetsByStatusResponse
     {
         // TODO: Implement findPetsByStatus() method.
     }
 
-    public function findPetsByTags()
+    public function findPetsByTags(): responses\FindPetsByTagsResponse
     {
         // TODO: Implement findPetsByTags() method.
     }
 
-    public function getPetById(string $petId)
+    public function getPetById(string $petId): responses\GetPetByIdResponse
     {
         // TODO: Implement getPetById() method.
     }
 
-    public function updatePetWithForm(string $petId)
+    public function updatePetWithForm(string $petId): responses\UpdatePetWithFormResponse
     {
         // TODO: Implement updatePetWithForm() method.
     }
 
-    public function deletePet(string $petId)
+    public function deletePet(string $petId): responses\DeletePetResponse
     {
         // TODO: Implement deletePet() method.
     }
 
-    public function uploadFile(string $petId)
+    public function uploadFile(string $petId): responses\UploadFileResponse
     {
         // TODO: Implement uploadFile() method.
     }
 
-    public function getInventory()
+    public function getInventory(): responses\GetInventoryResponse
     {
-        // TODO: Implement getInventory() method.
     }
 
-    public function placeOrder(?Order $body)
+    public function placeOrder(?schemas\Order $body): responses\PlaceOrderResponse
     {
         // TODO: Implement placeOrder() method.
     }
 
-    public function getOrderById(string $orderId)
+    public function getOrderById(string $orderId): responses\GetOrderByIdResponse
     {
         // TODO: Implement getOrderById() method.
     }
 
-    public function deleteOrder(string $orderId)
+    public function deleteOrder(string $orderId): responses\DeleteOrderResponse
     {
         // TODO: Implement deleteOrder() method.
     }
 
-    public function createUser(?User $body)
+    public function createUser(?schemas\User $body): responses\CreateUserResponse
     {
         // TODO: Implement createUser() method.
     }
 
     /**
-     * @param list<User>|null $body
+     * @param list<schemas\User>|null $body
      */
-    public function createUsersWithListInput(?array $body)
+    public function createUsersWithListInput(?array $body): responses\CreateUsersWithListInputResponse
     {
         // TODO: Implement createUsersWithListInput() method.
     }
 
-    public function loginUser()
+    public function loginUser(): responses\LoginUserResponse
     {
         // TODO: Implement loginUser() method.
     }
 
-    public function logoutUser()
+    public function logoutUser(): responses\LogoutUserResponse
     {
-        // TODO: Implement logoutUser() method.
+        return new \PetStore\LogoutUserResponseDefault(200);
     }
 
-    public function getUserByName(string $username)
+    public function getUserByName(string $username): responses\GetUserByNameResponse
     {
         // TODO: Implement getUserByName() method.
     }
 
-    public function updateUser(string $username, ?User $body)
+    public function updateUser(string $username, ?schemas\User $body): responses\UpdateUserResponse
     {
         // TODO: Implement updateUser() method.
     }
 
-    public function deleteUser(string $username)
+    public function deleteUser(string $username): responses\DeleteUserResponse
     {
         // TODO: Implement deleteUser() method.
     }
@@ -130,4 +129,4 @@ $petStore = new PetStore\APIClient(
     new Psr17Factory(),
     "https://petstore3.swagger.io/api/v3"
 );
-var_dump($petStore->getInventory());
+$petStore->logoutUser();
